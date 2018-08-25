579b1d4baef84d485c002c2b8c8fabf78f07433a
prashant-humney <prashant.humney89@gmail.com>
2018-08-25 16:33:15 +0530

    Initial Commit

--- ResumeBuilder/ViewController.swift
+++ ResumeBuilder/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  @IBOutlet weak var introText: UILabel! {
+    didSet {
+      self.introText.text = "Welcome to Resume Builder"
     }
+  }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
 
