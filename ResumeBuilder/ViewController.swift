881ad5bed39b4c9f15b57a88cf9ecdd3c73e41c5
prashant-humney <prashant.humney89@gmail.com>
2018-08-25 16:21:47 +0530

    Initial Commit

---
+++ ResumeBuilder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResumeBuilder
+//
+//  Created by Prashant Humney on 25/08/18.
+//  Copyright © 2018 Prashant Humney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
